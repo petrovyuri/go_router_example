6e351a4053337406fee4ad021549f82ae9869fe7
Petrov Yura <y.petrov@friflex.com>
2024-11-29 16:19:16 +0300

    feat(main): Переход на webview через диплинк

--- lib/features/main/main_routes.dart
+++ lib/features/main/main_routes.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:go_router_example/features/main/screens/main_screen.dart';
+import 'package:go_router_example/features/webview/webview_screen.dart';
 
 abstract final class MainRoutes {
   static const String mainPath = '/main';
@@ -8,9 +9,15 @@ abstract final class MainRoutes {
   static StatefulShellBranch buildShellBranch() =>
       StatefulShellBranch(initialLocation: mainPath, routes: [
         GoRoute(
-          path: mainPath,
-          name: mainScreen,
-          builder: (context, state) => const MainScreen(),
-        )
+            path: mainPath,
+            name: mainScreen,
+            builder: (context, state) => const MainScreen(),
+            routes: [
+              GoRoute(
+                path: '/web_main',
+                name: 'web_main',
+                builder: (context, state) => const WebViewScreen(),
+              )
+            ])
       ]);
 }

73290c323c5d9c87a7d2c2b47c0edaf0870091c2
Petrov Yura <y.petrov@friflex.com>
2024-12-05 12:21:42 +0300

    add simple example

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,33 +1,57 @@
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
-import 'package:go_router_example/features/main/main_routes.dart';
-import 'package:go_router_example/features/root/root_routes.dart';
-import 'package:go_router_example/features/webview/webview_routes.dart';
+
+import 'package:go_router_example/main.dart';
 
 /// Класс, реализующий роутер приложения и все поля классов
 class AppRouter {
-  AppRouter._();
-  static final _instance = AppRouter._();
-  static AppRouter get instance => _instance;
-
   /// Ключ для доступа к корневому навигатору приложения
-  final rootNavigatorKey = GlobalKey<NavigatorState>();
-
-  /// Начальный роут приложения
-  String get initialLocation => MainRoutes.mainPath;
+  static final rootNavigatorKey = GlobalKey<NavigatorState>();
 
   /// Метод для инициализации экземпляра GoRouter
   GoRouter initRouter() {
     final routes = [
-      // Нижнее навигационное меню
-      RootRoutes.build(rootNavigatorKey),
+      StatefulShellRoute.indexedStack(
+        parentNavigatorKey: rootNavigatorKey,
+        builder: (_, __, navigationShell) =>
+            RootScreen(navigationShell: navigationShell),
+        branches: [
+          StatefulShellBranch(initialLocation: '/main', routes: [
+            GoRoute(
+              path: '/main',
+              name: 'main_screen',
+              builder: (context, state) => const MainScreen(),
+            )
+          ]),
+          StatefulShellBranch(
+            initialLocation: '/auth',
+            routes: [
+              GoRoute(
+                path: '/auth',
+                name: 'auth_screen',
+                builder: (context, state) => const AuthScreen(),
+              ),
+            ],
+          )
+        ],
+      ),
       // Отдельный роут
-      WebviewRoutes.build(),
+      GoRoute(
+          path: '/web_view_screen',
+          name: 'web_view_screen',
+          builder: (context, state) => const WebViewScreen(),
+          routes: [
+            GoRoute(
+              path: '/confirm_screen',
+              name: 'confirm_screen',
+              builder: (context, state) => const ConfirmScreen(),
+            )
+          ])
     ];
     return GoRouter(
       debugLogDiagnostics: true,
       navigatorKey: rootNavigatorKey,
-      initialLocation: initialLocation,
+      initialLocation: '/main',
       routes: routes,
     );
   }

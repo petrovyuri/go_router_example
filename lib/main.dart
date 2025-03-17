ab57db0657025b097a4a5901768dcb32ee27ef0c
petrovyuri <y.petrov@friflex.com>
2025-03-17 11:29:59 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 void main() {
-  const link = '/global';
-  goRouter.go(link);
-
   runApp(MaterialApp.router(
     title: 'go_router demo',
     routerConfig: goRouter,
   ));
-}
 
-class MyNavigatorObserver extends NavigatorObserver {
-  MyNavigatorObserver();
-
-  @override
-  void didPop(Route<dynamic> route, Route<dynamic>? previousRoute) {
-    if (previousRoute?.settings.name == '/') {
-      goRouter.go('/');
-    }
-    super.didPop(route, previousRoute);
-  }
+  WidgetsBinding.instance.addPostFrameCallback((_) {
+    goRouter.push('/global'); 
+  });
 }
 
 final rootNavigatorKey = GlobalKey<NavigatorState>();
@@ -28,49 +17,48 @@ final rootNavigatorKey = GlobalKey<NavigatorState>();
 GoRouter goRouter = GoRouter(
     debugLogDiagnostics: true,
     navigatorKey: rootNavigatorKey,
-    observers: [MyNavigatorObserver()],
+    // observers: [MyNavigatorObserver()],
     initialLocation: '/',
     routes: [
       GoRoute(
-      path: '/',
-          builder: (context, state) => const RootScreen(),
+          path: '/',
           redirect: (context, state) {
             if (state.fullPath == '/') {
               return '/main';
             }
             return null;
           },
-      routes: [
-        StatefulShellRoute.indexedStack(
-          parentNavigatorKey: rootNavigatorKey,
-          builder: (_, __, navigationShell) =>
-              ShellScreen(navigationShell: navigationShell),
-          branches: [
-            StatefulShellBranch(
+          routes: [
+            StatefulShellRoute.indexedStack(
+              parentNavigatorKey: rootNavigatorKey,
+              builder: (_, __, navigationShell) =>
+                  ShellScreen(navigationShell: navigationShell),
+              branches: [
+                StatefulShellBranch(
                   initialLocation: '/main',
-              routes: [
-                GoRoute(
-                  path: '/main',
-                  name: 'main_screen',
-                  builder: (context, state) => const MainScreen(),
+                  routes: [
+                    GoRoute(
+                      path: '/main',
+                      name: 'main_screen',
+                      builder: (context, state) => const MainScreen(),
+                    )
+                  ],
+                ),
+                StatefulShellBranch(
+                  initialLocation: '/auth',
+                  routes: [
+                    GoRoute(
+                      path: '/auth',
+                      name: 'auth_screen',
+                      builder: (context, state) => const AuthScreen(),
+                    ),
+                  ],
                 )
               ],
             ),
-            StatefulShellBranch(
-                  initialLocation: '/auth',
-              routes: [
-                GoRoute(
-                  path: '/auth',
-                  name: 'auth_screen',
-                  builder: (context, state) => const AuthScreen(),
-                ),
-              ],
-            )
-          ],
-        ),
-    GoRoute(
-      path: '/global',
-      name: 'global_screen',
+            GoRoute(
+              path: '/global',
+              name: 'global_screen',
               builder: (context, state) => const GlobalScreen(),
             )
           ])
